f87f011c2d537f51bf949012b5851910646e30f4
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-04-26 19:17:42 +0900

    ACコード

---
+++ ABC144D.go
@@ -0,0 +1,23 @@
+package gocoder
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b, x float64
+	fmt.Scanf("%g %g %g", &a, &b, &x)
+
+	var rad float64
+	if a*a*b/2 >= x {
+		h := x / (b * a / 2)
+		rad = math.Atan(b / h)
+	} else {
+		b = 2 * (b - x/(a*a))
+		rad = math.Atan(b / a)
+	}
+
+	ans := rad * 180 / math.Pi
+	fmt.Println(ans)
+}
